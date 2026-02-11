262998a837007bbc79bd9ac6b6e90bc15c720d9a
Sbrld <sabyraldo@gmail.com>
2026-02-11 13:22:59 +0500

    update

---
+++ lab3/task1/JavaScript Fundamentals/2.10.exercise(2).js
@@ -0,0 +1,11 @@
+'use strict';
+
+    let value = prompt("Enter a digit");
+
+    if (value > 0) {
+      alert(1);
+    } else if (value == 0){
+      alert(0);
+    } else {
+        alert(-1);
+    }
